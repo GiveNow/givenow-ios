1f14c47ea44baad0477c374de346efbf4cf046e3
brennanMKE <brennan@smallsharptools.com>
2015-12-17 01:11:12 -0800

    added undo command to assume unchanged script

--- assume_unchanged.sh
+++ assume_unchanged.sh
@@ -2,3 +2,6 @@
 
 git update-index --assume-unchanged GiveNow/GiveNow/Keys.plist
 
+# Undo the change above with the command below
+# git update-index --no-assume-unchanged GiveNow/GiveNow/Keys.plist
+
