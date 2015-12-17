80658ade0d257740dc453aaae514b77e149a39ed
Brian Beckerle <bribeck1@gmail.com>
2015-12-16 21:25:37 -0800

    Paged collection view skeleton

--- GiveNow/GiveNow/OnboardingViewController.swift
+++ GiveNow/GiveNow/OnboardingViewController.swift
@@ -12,8 +12,12 @@ import UIKit
 // See https://github.com/GiveNow/givenow-ios/issues/4
 // See https://github.com/GiveNow/givenow-ios/issues/5
 
-class OnboardingViewController: BaseViewController {
+class OnboardingViewController: BaseViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var pageControl : UIPageControl!
+    
+    let backgroundColors = [UIColor.greenColor(), UIColor.orangeColor(), UIColor.blueColor(), UIColor.purpleColor(), UIColor.yellowColor()]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,15 +29,35 @@ class OnboardingViewController: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: UICollectionViewDelegateFlowLayout
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return self.view.frame.size
     }
-    */
+    
+    //MARK: UICollectionViewDataSource
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        if indexPath.row < backgroundColors.count {
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("OnboardingCell", forIndexPath: indexPath)
+            cell.backgroundColor = backgroundColors[indexPath.row]
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier("SignInCell", forIndexPath: indexPath)
+            cell.backgroundColor = UIColor.magentaColor()
+            return cell
+        }
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return backgroundColors.count + 1 //Sign In Cell
+    }
+    
+    //MARK: UICollectionViewDelegate
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+        let pageWidth = self.view.frame.size.width
+        self.pageControl.currentPage = Int(scrollView.contentOffset.x / pageWidth)
+    }
 
 }
