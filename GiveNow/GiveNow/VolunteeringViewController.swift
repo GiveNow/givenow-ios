4744248554dff70096ee3d4a489fcdaeeb9b2e78
Evan Waters <evan.waters@gmail.com>
2015-12-17 20:03:03 -0800

    Created UIColor extensions for core colors; set up basic view controller structure for volunteer workflow

--- GiveNow/GiveNow/VolunteeringViewController.swift
+++ GiveNow/GiveNow/VolunteeringViewController.swift
@@ -17,9 +17,14 @@ class VolunteeringViewController: BaseViewController {
     
     @IBOutlet weak var volunteeringTitleLabel: UILabel!
     @IBOutlet weak var volunteerButton: UIButton!
+    
+    // Assume this will be retrieved from logged in user object
+    var userPhoneNumber:String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initializeLabels()
 
         // Do any additional setup after loading the view.
     }
@@ -28,6 +33,60 @@ class VolunteeringViewController: BaseViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func initializeLabels() {
+        //ToDo: Localize
+        volunteeringTitleLabel.text = "Want to volunteer to pick up donations? The only thing you need is a ar and some spare time!"
+        volunteerButton.setTitle("I want to volunteer!", forState: .Normal)
+    }
+    
+    
+    @IBAction func volunteerButtonTapped(sender: AnyObject) {
+        
+        // if user logged in, submit volunteer request
+        // and then update the label and text
+        if AppState.sharedInstance().isUserLoggedIn {
+            submitVolunteerApplicationRequest()
+        }
+            
+        // if not, do login flow and then submit volunteer request
+        else {
+            askUserToLogin()
+        }
+        
+    }
+    
+    func submitVolunteerApplicationRequest() {
+        //ToDo: Create object, submit to back end
+        
+        // after successful submission
+        updateViewAfterSuccessfulSubmission()
+    }
+    
+    func askUserToLogin() {
+        //Display modal login dialogue
+        
+        // after successful login
+        submitVolunteerApplicationRequest()
+    }
+    
+    func updateViewAfterSuccessfulSubmission() {
+        //ToDo: Localize
+        volunteeringTitleLabel.text = "Thanks for applying to volunteer! We'll contact you soon at \(userPhoneNumber)"
+        volunteerButton.setTitle("You applied to volunteer", forState: .Normal)
+        volunteerButton.backgroundColor = UIColor.lightGrayColor()
+        
+    }
+    
+//// Could use this to check if user is logged in
+//    func isLoggedIn() -> Bool {
+//        let currentUser = PFUser.currentUser()
+//        if currentUser != nil {
+//            return true
+//        } else {
+//           return false
+//        }
+//    }
     
     
     
