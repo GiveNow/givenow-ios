1e6cd69144b7ceeadfa21ff6dc31465d939f83ba
Evan Waters <evan.waters@gmail.com>
2016-01-12 15:43:42 -0800

    Implemented dropoff menu button

---
+++ GiveNow/GiveNow/DropOffViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DropOffViewController.swift
+//  GiveNow
+//
+//  Created by Evan Waters on 1/12/16.
+//  Copyright © 2016 GiveNow. All rights reserved.
+//
+
+import UIKit
+
+class DropOffViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeMenuButton()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func initializeMenuButton() {
+        if self.revealViewController() != nil {
+            self.menuButton.target = self.revealViewController()
+            self.menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+}
